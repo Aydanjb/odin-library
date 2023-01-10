14566aab7b51229317ccc3bdccf1470372fc4161
aydan <AydanBondurant@gmail.com>
2023-01-09 19:04:26 -0500

    filled array with books

--- script.js
+++ script.js
@@ -14,5 +14,11 @@ function addBookToLibrary(book) {
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295", "Read");
+const lotr1 = new Book("The Fellowship of the Ring", "J.R.R Tolkien", "527", "Read");
+const lotr2 = new Book("The Two Towers", "J.R.R Tolkien", "448", "Not read")
+const lotr3 = new Book("Return of the King", "J.R.R Tolkien", "432", "Not read");
 
 addBookToLibrary(theHobbit);
+addBookToLibrary(lotr1);
+addBookToLibrary(lotr2);
+addBookToLibrary(lotr3);
